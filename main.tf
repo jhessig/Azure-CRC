b6ec13968671805df45dd1ed6986dd3d849aaf70
Jeremy Hessig <jeremy@hessig.contact>
2024-06-08 15:10:07 -0400

    Run terraform fmt.
    Fixed formatting.

--- main.tf
+++ main.tf
@@ -92,17 +92,17 @@ resource "azurerm_dns_cname_record" "cname" {
 }
 
 resource "azurerm_cdn_endpoint_custom_domain" "www" {
-  count = var.env_tag == "prod" ? 1 : 0
-  name = "www-domain"
-  host_name = "www.${var.sld_name}.${var.tld_name}"
+  count           = var.env_tag == "prod" ? 1 : 0
+  name            = "www-domain"
+  host_name       = "www.${var.sld_name}.${var.tld_name}"
   cdn_endpoint_id = azurerm_cdn_endpoint.cdn_endpoint.id
 }
 
 resource "azurerm_cdn_endpoint_custom_domain" "root" {
-  count = var.env_tag == "prod" ? 1 : 0
-  name                = "root-domain"
-  host_name           = "${var.sld_name}.${var.tld_name}"
-  cdn_endpoint_id     = azurerm_cdn_endpoint.cdn_endpoint.id
+  count           = var.env_tag == "prod" ? 1 : 0
+  name            = "root-domain"
+  host_name       = "${var.sld_name}.${var.tld_name}"
+  cdn_endpoint_id = azurerm_cdn_endpoint.cdn_endpoint.id
 }
 
 output "storage_url" {
