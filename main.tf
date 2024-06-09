15774250e94683fc83fae77eb254283fae6a714b
Jeremy Hessig <jeremy@hessig.contact>
2024-06-08 20:10:51 -0400

    Add dependencies to Azure CDN domain resources
    The commit adds two new dependencies to the Azure CDN `www-domain` and `root-domain` resources in the Terraform configuration. The `www-domain` resource now depends on `azurerm_dns_cname_record.cname`, and the `root-domain` resource now depends on `azurerm_dns_a_record.a_root`. These added dependencies ensure the correct order of operations when creating or modifying Azure CDN and DNS resources.

--- main.tf
+++ main.tf
@@ -104,6 +104,7 @@ resource "azurerm_cdn_endpoint_custom_domain" "www" {
   name            = "www-domain"
   host_name       = "www.${var.sld_name}.${var.tld_name}"
   cdn_endpoint_id = azurerm_cdn_endpoint.cdn_endpoint.id
+  depends_on      = [azurerm_dns_cname_record.cname]
 }
 
 resource "azurerm_cdn_endpoint_custom_domain" "root" {
@@ -111,6 +112,7 @@ resource "azurerm_cdn_endpoint_custom_domain" "root" {
   name            = "root-domain"
   host_name       = "${var.sld_name}.${var.tld_name}"
   cdn_endpoint_id = azurerm_cdn_endpoint.cdn_endpoint.id
+  depends_on      = [azurerm_dns_a_record.a_root]
 }
 
 output "storage_url" {
