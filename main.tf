9d0dfe1715395beef0d7d76ad1a97e05c920ceba
Jeremy Hessig <jeremy@hessig.contact>
2024-06-14 18:56:43 -0400

    Update checkov skip labels in main.tf
    The checkov skip labels for CKV_AZURE_59 and CKV_AZURE_190 in the main.tf file were corrected. Previous labels, CKV2_AZURE_59 and CKV2_AZURE_190, were mistakenly used.

--- main.tf
+++ main.tf
@@ -46,8 +46,8 @@ resource "azurerm_storage_account" "storage" {
   #checkov:skip=CKV2_AZURE_33:The storage account is a public static content host.
   #checkov:skip=CKV2_AZURE_40:The storage account is a public static content host.
   #checkov:skip=CKV2_AZURE_47:The storage account is a public static content host.
-  #checkov:skip=CKV2_AZURE_59:The storage account is a public static content host.
-  #checkov:skip=CKV2_AZURE_190:The storage account is a public static content host.
+  #checkov:skip=CKV_AZURE_59:The storage account is a public static content host.
+  #checkov:skip=CKV_AZURE_190:The storage account is a public static content host.
   account_replication_type = "LRS"
   account_tier             = "Standard"
   location                 = azurerm_resource_group.rg.location
