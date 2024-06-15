85deb13fdef45db5e116c53e6536e4aff705d710
Jeremy Hessig <jeremy@hessig.contact>
2024-06-15 14:42:17 -0400

    Add Azure storage account and function app resources
    New Azure resources including a storage account and a function app have been provisioned. The storage account is configured with parameters for a blob's delete retention policy and queue properties. Furthermore, the function app is generated with an associated app service plan and specific runtime settings.

--- main.tf
+++ main.tf
@@ -127,7 +127,7 @@ resource "azurerm_dns_a_record" "a_root" {
   target_resource_id  = azurerm_cdn_endpoint.cdn_endpoint.id
 }
 
-resource "azurerm_dns_cname_record" "www-cname" {
+resource "azurerm_dns_cname_record" "www_cname" {
   name                = "www"
   zone_name           = azurerm_dns_zone.zone.name
   resource_group_name = azurerm_resource_group.rg.name
@@ -135,7 +135,7 @@ resource "azurerm_dns_cname_record" "www-cname" {
   record              = azurerm_cdn_endpoint.cdn_endpoint.fqdn
 }
 
-resource "azurerm_dns_cname_record" "cdn-cname" {
+resource "azurerm_dns_cname_record" "cdn_cname" {
   name                = "cdnverify"
   resource_group_name = azurerm_resource_group.rg.name
   ttl                 = 300
@@ -144,7 +144,7 @@ resource "azurerm_dns_cname_record" "cdn-cname" {
 }
 
 ### Set up API.
-resource "azurerm_resource_group" "api-rg" {
+resource "azurerm_resource_group" "api_rg" {
   name     = "${var.resource_group_name}-${var.env_tag}-api-rg"
   location = "eastus"
   tags = {
@@ -153,10 +153,10 @@ resource "azurerm_resource_group" "api-rg" {
 }
 
 resource "azurerm_cosmosdb_account" "cosmosdb" {
-  location            = azurerm_resource_group.api-rg.location
+  location            = azurerm_resource_group.api_rg.location
   name                = "${var.resource_group_name}-cosmos-${var.env_tag}-${random_string.storage_id.result}"
   offer_type          = "Standard"
-  resource_group_name = azurerm_resource_group.api-rg.name
+  resource_group_name = azurerm_resource_group.api_rg.name
   kind                = "GlobalDocumentDB"
   consistency_policy {
     consistency_level = "Session"
@@ -173,6 +173,75 @@ resource "azurerm_cosmosdb_account" "cosmosdb" {
   }
 }
 
+resource "azurerm_storage_account" "api_storage" {
+  account_replication_type = "GRS"
+  account_tier             = "Standard"
+  location                 = azurerm_resource_group.api_rg.location
+  name                     = "${var.resource_group_name}apistorage${random_string.storage_id.result}"
+  resource_group_name      = azurerm_resource_group.api_rg.name
+  min_tls_version          = "TLS1_2"
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    hour_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    minute_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+}
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "azure-functions-${var.resource_group_name}-${var.env_tag}"
+  location            = azurerm_resource_group.api_rg.location
+  resource_group_name = azurerm_resource_group.api_rg.name
+  kind                = "Linux"
+  reserved            = true
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+  lifecycle {
+    ignore_changes = [
+      kind
+    ]
+  }
+}
+
+resource "azurerm_function_app" "function_app" {
+  name                       = "${var.resource_group_name}-${var.env_tag}-azure-function"
+  location                   = azurerm_resource_group.api_rg.location
+  resource_group_name        = azurerm_resource_group.api_rg.name
+  app_service_plan_id        = azurerm_app_service_plan.app_service_plan.id
+  storage_account_name       = azurerm_storage_account.api_storage.name
+  storage_account_access_key = azurerm_storage_account.api_storage.primary_access_key
+  os_type                    = "linux"
+  version                    = "~4"
+  app_settings {
+    FUNCTIONS_WORKER_RUNTIME = "python"
+  }
+  site_config {
+    linux_fx_version = "python|3.11"
+  }
+}
+
 ### Set CDN custom domains.
 resource "azurerm_cdn_endpoint_custom_domain" "www" {
   count           = var.env_tag == "prod" ? 1 : 0
@@ -183,7 +252,7 @@ resource "azurerm_cdn_endpoint_custom_domain" "www" {
     certificate_type = "Dedicated"
     protocol_type    = "ServerNameIndication"
   }
-  depends_on = [azurerm_dns_cname_record.www-cname]
+  depends_on = [azurerm_dns_cname_record.www_cname]
 }
 
 resource "azurerm_cdn_endpoint_custom_domain" "root" {
@@ -191,7 +260,7 @@ resource "azurerm_cdn_endpoint_custom_domain" "root" {
   name            = "root-domain"
   host_name       = "${var.sld_name}.${var.tld_name}"
   cdn_endpoint_id = azurerm_cdn_endpoint.cdn_endpoint.id
-  depends_on      = [azurerm_dns_cname_record.cdn-cname]
+  depends_on      = [azurerm_dns_cname_record.cdn_cname]
 }
 
 output "storage_url" {
