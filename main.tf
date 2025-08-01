035995bdc2288b471288cb1217c4db2f8eae19c4
Jeremy Hessig <jeremy@hessig.contact>
2025-08-01 17:11:29 -0400

    Fix incorrect DNS zone and resource group references in Terraform configuration

--- main.tf
+++ main.tf
@@ -153,7 +153,7 @@ resource "azurerm_key_vault_secret" "cdn_endpoint" {
 resource "azurerm_dns_a_record" "a_root" {
   count               = var.env_tag == "prod" ? 1 : 0
   name                = "@"
-  zone_name           = azurerm_dns_zone.zone.name[0]
+  zone_name           = azurerm_dns_zone.zone[count.index].name
   resource_group_name = azurerm_resource_group.rg.name
   ttl                 = 300
   target_resource_id  = azurerm_cdn_endpoint.cdn_endpoint.id
@@ -162,7 +162,7 @@ resource "azurerm_dns_a_record" "a_root" {
 resource "azurerm_dns_cname_record" "www_cname" {
   count               = var.env_tag == "prod" ? 1 : 0
   name                = "www"
-  zone_name           = azurerm_dns_zone.zone.name[0]
+  zone_name           = azurerm_dns_zone.zone[count.index].name
   resource_group_name = azurerm_resource_group.rg.name
   ttl                 = 300
   record              = azurerm_cdn_endpoint.cdn_endpoint.fqdn
@@ -171,7 +171,7 @@ resource "azurerm_dns_cname_record" "www_cname" {
 resource "azurerm_dns_cname_record" "cdn_cname" {
   count               = var.env_tag == "prod" ? 1 : 0
   name                = "cdnverify"
-  resource_group_name = azurerm_resource_group.rg.name[0]
+  resource_group_name = azurerm_dns_zone.zone[count.index].name
   ttl                 = 300
   zone_name           = azurerm_dns_zone.zone.name
   record              = "cdnverify.${azurerm_cdn_endpoint.cdn_endpoint.fqdn}"
