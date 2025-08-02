323f630869b8a709018a38c2a468199aa45def4e
Jeremy Hessig <jeremy@hessig.contact>
2025-08-02 12:25:24 -0400

    Enhance CORS headers in Azure Function App response configuration

--- function-app/function_app.py
+++ function-app/function_app.py
@@ -40,7 +40,7 @@ def visitor_count(req: func.HttpRequest) -> func.HttpResponse:
             status_code=200,
             headers={
                 "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*"
+                "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
                 "Access-Control-Allow-Headers": "Content-Type"
             }
