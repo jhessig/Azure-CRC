fae9b4dee46295dddd4b5a665d1b100cf1ecf47f
Jeremy Hessig <jeremy@hessig.contact>
2025-08-03 00:16:58 -0400

    Update `visitor_count` route to use anonymous authentication in Azure Function App

--- function-app/function_app.py
+++ function-app/function_app.py
@@ -5,7 +5,7 @@ from azure.data.tables import TableServiceClient
 from azure.core.exceptions import ResourceNotFoundError
 
 app = func.FunctionApp()
-@app.route(route="visitor_count", auth_level=func.AuthLevel.FUNCTION)
+@app.route(route="visitor_count", auth_level=func.AuthLevel.ANONYMOUS)
 def visitor_count(req: func.HttpRequest) -> func.HttpResponse:
     try:
         # Get connection string
