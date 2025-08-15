6a984758135c1ee907e3bf21f57fbe062e45548d
Jeremy Hessig <jeremy@hessig.contact>
2025-08-15 09:31:41 -0400

    Re-enable DNS record resources in `main.tf` and comment out CDN custom domain resources due to deployment inconsistencies; manual setup required post-deployment.

--- main.tf
+++ main.tf
@@ -110,34 +110,33 @@ resource "azurerm_cdn_endpoint" "cdn_endpoint" {
   optimization_type = "GeneralWebDelivery"
 }
 
-### Disabling due to inconsistency with DNS name server assignment.
-### Manual deployment of records required after deployment.
-# resource "azurerm_dns_a_record" "a_root" {
-#   count               = var.env_tag == "prod" ? 1 : 0
-#   name                = "@"
-#   zone_name           = azurerm_dns_zone.zone[count.index].name
-#   resource_group_name = azurerm_resource_group.rg.name
-#   ttl                 = 300
-#   target_resource_id  = azurerm_cdn_endpoint.cdn_endpoint.id
-# }
-#
-# resource "azurerm_dns_cname_record" "www_cname" {
-#   count               = var.env_tag == "prod" ? 1 : 0
-#   name                = "www"
-#   zone_name           = azurerm_dns_zone.zone[count.index].name
-#   resource_group_name = azurerm_resource_group.rg.name
-#   ttl                 = 300
-#   record              = azurerm_cdn_endpoint.cdn_endpoint.fqdn
-# }
-#
-# resource "azurerm_dns_cname_record" "cdn_cname" {
-#   count               = var.env_tag == "prod" ? 1 : 0
-#   name                = "cdnverify"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   ttl                 = 300
-#   zone_name           = azurerm_dns_zone.zone[count.index].name
-#   record              = "cdnverify.${azurerm_cdn_endpoint.cdn_endpoint.fqdn}"
-# }
+
+resource "azurerm_dns_a_record" "a_root" {
+  count               = var.env_tag == "prod" ? 1 : 0
+  name                = "@"
+  zone_name           = azurerm_dns_zone.zone[count.index].name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+  target_resource_id  = azurerm_cdn_endpoint.cdn_endpoint.id
+}
+
+resource "azurerm_dns_cname_record" "www_cname" {
+  count               = var.env_tag == "prod" ? 1 : 0
+  name                = "www"
+  zone_name           = azurerm_dns_zone.zone[count.index].name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+  record              = azurerm_cdn_endpoint.cdn_endpoint.fqdn
+}
+
+resource "azurerm_dns_cname_record" "cdn_cname" {
+  count               = var.env_tag == "prod" ? 1 : 0
+  name                = "cdnverify"
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+  zone_name           = azurerm_dns_zone.zone[count.index].name
+  record              = "cdnverify.${azurerm_cdn_endpoint.cdn_endpoint.fqdn}"
+}
 
 ### Set up API.
 resource "azurerm_resource_group" "api_rg" {
@@ -362,26 +361,27 @@ resource "azurerm_monitor_metric_alert" "high_request_volume" {
   window_size = "PT5M"
 }
 
-### Set CDN custom domains.
-resource "azurerm_cdn_endpoint_custom_domain" "www" {
-  count           = var.env_tag == "prod" ? 1 : 0
-  name            = "www-domain"
-  host_name       = "www.${var.sld_name}.${var.tld_name}"
-  cdn_endpoint_id = azurerm_cdn_endpoint.cdn_endpoint.id
-  cdn_managed_https {
-    certificate_type = "Dedicated"
-    protocol_type    = "ServerNameIndication"
-  }
-  depends_on = [azurerm_dns_cname_record.www_cname]
-}
-
-resource "azurerm_cdn_endpoint_custom_domain" "root" {
-  count           = var.env_tag == "prod" ? 1 : 0
-  name            = "root-domain"
-  host_name       = "${var.sld_name}.${var.tld_name}"
-  cdn_endpoint_id = azurerm_cdn_endpoint.cdn_endpoint.id
-  depends_on      = [azurerm_dns_cname_record.cdn_cname]
-}
+### Disabling due to inconsistency with DNS name server assignment.
+### Manual deployment of endpoint custom domains required after deployment.
+# resource "azurerm_cdn_endpoint_custom_domain" "www" {
+#   count           = var.env_tag == "prod" ? 1 : 0
+#   name            = "www-domain"
+#   host_name       = "www.${var.sld_name}.${var.tld_name}"
+#   cdn_endpoint_id = azurerm_cdn_endpoint.cdn_endpoint.id
+#   cdn_managed_https {
+#     certificate_type = "Dedicated"
+#     protocol_type    = "ServerNameIndication"
+#   }
+#   depends_on = [azurerm_dns_cname_record.www_cname]
+# }
+#
+# resource "azurerm_cdn_endpoint_custom_domain" "root" {
+#   count           = var.env_tag == "prod" ? 1 : 0
+#   name            = "root-domain"
+#   host_name       = "${var.sld_name}.${var.tld_name}"
+#   cdn_endpoint_id = azurerm_cdn_endpoint.cdn_endpoint.id
+#   depends_on      = [azurerm_dns_cname_record.cdn_cname]
+# }
 
 resource "azurerm_key_vault_secret" "resource_group" {
   name            = "${var.env_tag}-resource-group"
