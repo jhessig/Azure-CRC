1390dc6b049fd421cb79c716da4f4bad54f799f3
Jeremy Hessig <jeremy@hessig.contact>
2025-08-15 09:22:04 -0400

    Comment out DNS record resources in `main.tf` due to deployment inconsistencies; manual setup required post-deployment.

--- main.tf
+++ main.tf
@@ -110,32 +110,34 @@ resource "azurerm_cdn_endpoint" "cdn_endpoint" {
   optimization_type = "GeneralWebDelivery"
 }
 
-resource "azurerm_dns_a_record" "a_root" {
-  count               = var.env_tag == "prod" ? 1 : 0
-  name                = "@"
-  zone_name           = azurerm_dns_zone.zone[count.index].name
-  resource_group_name = azurerm_resource_group.rg.name
-  ttl                 = 300
-  target_resource_id  = azurerm_cdn_endpoint.cdn_endpoint.id
-}
-
-resource "azurerm_dns_cname_record" "www_cname" {
-  count               = var.env_tag == "prod" ? 1 : 0
-  name                = "www"
-  zone_name           = azurerm_dns_zone.zone[count.index].name
-  resource_group_name = azurerm_resource_group.rg.name
-  ttl                 = 300
-  record              = azurerm_cdn_endpoint.cdn_endpoint.fqdn
-}
-
-resource "azurerm_dns_cname_record" "cdn_cname" {
-  count               = var.env_tag == "prod" ? 1 : 0
-  name                = "cdnverify"
-  resource_group_name = azurerm_resource_group.rg.name
-  ttl                 = 300
-  zone_name           = azurerm_dns_zone.zone[count.index].name
-  record              = "cdnverify.${azurerm_cdn_endpoint.cdn_endpoint.fqdn}"
-}
+### Disabling due to inconsistency with DNS name server assignment.
+### Manual deployment of records required after deployment.
+# resource "azurerm_dns_a_record" "a_root" {
+#   count               = var.env_tag == "prod" ? 1 : 0
+#   name                = "@"
+#   zone_name           = azurerm_dns_zone.zone[count.index].name
+#   resource_group_name = azurerm_resource_group.rg.name
+#   ttl                 = 300
+#   target_resource_id  = azurerm_cdn_endpoint.cdn_endpoint.id
+# }
+#
+# resource "azurerm_dns_cname_record" "www_cname" {
+#   count               = var.env_tag == "prod" ? 1 : 0
+#   name                = "www"
+#   zone_name           = azurerm_dns_zone.zone[count.index].name
+#   resource_group_name = azurerm_resource_group.rg.name
+#   ttl                 = 300
+#   record              = azurerm_cdn_endpoint.cdn_endpoint.fqdn
+# }
+#
+# resource "azurerm_dns_cname_record" "cdn_cname" {
+#   count               = var.env_tag == "prod" ? 1 : 0
+#   name                = "cdnverify"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   ttl                 = 300
+#   zone_name           = azurerm_dns_zone.zone[count.index].name
+#   record              = "cdnverify.${azurerm_cdn_endpoint.cdn_endpoint.fqdn}"
+# }
 
 ### Set up API.
 resource "azurerm_resource_group" "api_rg" {
